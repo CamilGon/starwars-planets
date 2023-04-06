bd103f0778f65fd59525718fb67b4a99b38bf160
CamilaGon <cg561351@gmail.com>
2023-04-06 18:41:16 -0300

    Req 3 e inicio do 4

--- src/components/FiltersTable.js
+++ src/components/FiltersTable.js
@@ -2,7 +2,17 @@ import React, { useContext } from 'react';
 import PlanetContext from '../context/PlanetContext';
 
 export default function FiltersTable() {
-  const { filterByName, setfilterByName } = useContext(PlanetContext);
+  const {
+    filterByName,
+    setfilterByName,
+    column,
+    comparison,
+    valueFilter,
+    setColumn,
+    setComparison,
+    setValueFilter,
+    setFilterGroup,
+  } = useContext(PlanetContext);
   return (
     <form>
       <hr />
@@ -13,6 +23,45 @@ export default function FiltersTable() {
         onChange={ (e) => setfilterByName(e.target.value) }
         placeholder="Filter by name..."
       />
+      <select
+        data-testid="column-filter"
+        value={ column }
+        onChange={ (e) => setColumn(e.target.value) }
+      >
+        <option value="population">population</option>
+        <option value="orbital_period">orbital_period</option>
+        <option value="diameter">diameter</option>
+        <option value="otation_period">rotation_period</option>
+        <option value="surface_water">surface_water</option>
+      </select>
+      <select
+        data-testid="comparison-filter"
+        value={ comparison }
+        onChange={ (e) => setComparison(e.target.value) }
+      >
+        <option value="maior que">maior que</option>
+        <option value="menor que">menor que</option>
+        <option value="igual a">igual a</option>
+      </select>
+      <input
+        type="number"
+        data-testid="value-filter"
+        value={ valueFilter }
+        onChange={ (e) => setValueFilter(e.target.value) }
+      />
+      <button
+        type="button"
+        onClick={ () => {
+          setFilterGroup({
+            column,
+            comparison,
+            valueFilter,
+          });
+        } }
+        data-testid="button-filter"
+      >
+        Filtrar
+      </button>
       <hr />
     </form>
 
--- src/context/PlanetProvider.js
+++ src/context/PlanetProvider.js
@@ -6,6 +6,14 @@ function PlanetProvider({ children }) {
   const [listPlanets, setListPlanets] = useState([]);
   const [filterByName, setfilterByName] = useState('');
   const [planetsFiltered, setPlanetsFiltered] = useState(listPlanets);
+  const [column, setColumn] = useState('population');
+  const [comparison, setComparison] = useState('maior que');
+  const [valueFilter, setValueFilter] = useState(0);
+  const [filterGroup, setFilterGroup] = useState({
+    column: '',
+    comparison: '',
+    valueFilter: 0,
+  });
 
   useEffect(() => {
     const fetchPlanets = async () => {
@@ -13,23 +21,54 @@ function PlanetProvider({ children }) {
       const data = await response.json();
       const planets = data.results;
       setListPlanets(planets);
+      console.log();
       const newplanetsFiltered = planets
-        .filter((planet) => planet.name.includes(filterByName));
+        .filter((planet) => planet.name.includes(filterByName.toLowerCase()));
       setPlanetsFiltered(newplanetsFiltered);
     };
     fetchPlanets();
   }, [filterByName]);
 
+  useEffect(() => {
+    const comparisonBy = {
+      'maior que': (a, b) => a > b,
+      'menor que': (a, b) => a < b,
+      'igual a': (a, b) => a === b,
+    };
+
+    if (filterGroup.column) {
+      const newFilterGroup = listPlanets
+        .filter((planet) => comparisonBy[filterGroup.comparison](
+          Number(planet[filterGroup.column]),
+          Number(filterGroup.valueFilter),
+        ));
+      setPlanetsFiltered(newFilterGroup);
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filterGroup]);
+
   const values = useMemo(() => ({
     listPlanets,
     filterByName,
-    setfilterByName,
     planetsFiltered,
+    column,
+    comparison,
+    valueFilter,
+    filterGroup,
+    setfilterByName,
     setPlanetsFiltered,
+    setColumn,
+    setComparison,
+    setValueFilter,
+    setFilterGroup,
   }), [
     listPlanets,
     filterByName,
     planetsFiltered,
+    column,
+    comparison,
+    valueFilter,
+    filterGroup,
   ]);
   return (
     <PlanetContext.Provider value={ values }>
